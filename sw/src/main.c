fdca35d5668d067444e4758c829bb79d23a02757
Reto Stalder <stalder@kombu.ch>
2015-06-12 09:35:03 +0200

    Added value for configuration word register

--- sw/src/main.c
+++ sw/src/main.c
@@ -20,12 +20,30 @@
 //    You should have received a copy of the GNU General Public License
 //    along with Pedalier.  If not, see <http://www.gnu.org/licenses/>.
 
-
 // Include the SystemConfiguration interface
 #include "system_configuration.h"
 
 // Include the ApplicationRunner interface 
 #include "application_runner.h"
+
+
+/**************************************************************************
+ * Configuration Word Register
+ **************************************************************************/
+// 15..12: Reserved  0
+//     11: FCMEN     0     Failsafe clock monitor disabled
+//     10: IESO      0     Int/Ext switchover disabled
+// 09..08: BOREN     00    BOR disabled
+//                   = 0x00
+//     07: CPDn      1     Data memory code protection disabled
+//     06: CPn       1     Program memory code protection disabled
+//     05: MCLRE     0     MCLRn pin function is digital input
+//     04: PWRTEn    1     Power-up timer is disabled
+//     03: WDTE      0     Watchdog timer is disabled
+// 02..00: FOSC      100   INTOSC and I/O function on RA4/RA5
+//                   = 0xD4
+unsigned int at 0x2007 configWord = 0x00D4;
+
 
 void main() {
 	// Initialize the system
